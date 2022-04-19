31fec4e15afb1b7074b9650bc15c10ddccadc930
LUKASRIB15 <lucas.ribeiro06@aluno.ifce.edu.br>
2022-04-19 16:58:23 -0300

    cliente#1

--- backend/models.py
+++ backend/models.py
@@ -4,37 +4,42 @@ from flask_login import UserMixin
 
 
 @login_manager.user_loader
-def carregaUsuario(id):
-    return Usuario.query.get(id)
+def carregaCliente(id):
+    return Cliente.query.get(id)
 
-
-class Usuario(db.Model, UserMixin):
+class Cliente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(30), unique=True, nullable=False)
+    nome = db.Column(db.String(400), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullabel=False)
     senha = db.Column(db.String(100), nullable=False)
-    nome_completo = db.Column(db.String(400))
 
-    mensagens = db.relationship("Mensagem", backref="usuario", lazy=True)
+#class Usuario(db.Model, UserMixin):
+#   id = db.Column(db.Integer, primary_key=True)
+ #   nome = db.Column(db.String(30), unique=True, nullable=False)
+  #  senha = db.Column(db.String(100), nullable=False)
+   # nome_completo = db.Column(db.String(400))
 
-
-class Mensagem(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    texto = db.Column(db.String(144), nullable=False)
-
-    remetente = db.Column(
-        db.Integer, db.ForeignKey("usuario.id"), nullable=False
-    )
-    conversa_id = db.Column(db.Integer, db.ForeignKey("conversa.id"))
+    #mensagens = db.relationship("Mensagem", backref="usuario", lazy=True)
 
 
-class Conversa(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+#class Mensagem(db.Model):
+ #   id = db.Column(db.Integer, primary_key=True)
+  #  texto = db.Column(db.String(144), nullable=False)
 
-    part_a_id = db.Column(
-        db.Integer, db.ForeignKey("usuario.id"), nullable=False
-    )
-    part_b_id = db.Column(
-        db.Integer, db.ForeignKey("usuario.id"), nullable=False
-    )
+   # remetente = db.Column(
+    #    db.Integer, db.ForeignKey("usuario.id"), nullable=False
+    #)
+    #conversa_id = db.Column(db.Integer, db.ForeignKey("conversa.id"))
 
-    mensagens = db.relationship("Mensagem", backref="conversa", lazy=True)
+
+#class Conversa(db.Model):
+ #   id = db.Column(db.Integer, primary_key=True)
+
+#    part_a_id = db.Column(
+#        db.Integer, db.ForeignKey("usuario.id"), nullable=False
+ #   )
+  #  part_b_id = db.Column(
+   #     db.Integer, db.ForeignKey("usuario.id"), nullable=False
+    #)
+
+    #mensagens = db.relationship("Mensagem", backref="conversa", lazy=True)
