fbe20ad0f15d2cb2379a40ba4c7c14808a9d418a
LUKASRIB15 <lucas.ribeiro06@aluno.ifce.edu.br>
2022-04-20 12:54:31 +0000

    cliente#1 herança da classe UserMixin

--- backend/models.py
+++ backend/models.py
@@ -7,7 +7,7 @@ from flask_login import UserMixin
 def carregaCliente(id):
     return Cliente.query.get(id)
 
-class Cliente(db.Model):
+class Cliente(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(400), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
