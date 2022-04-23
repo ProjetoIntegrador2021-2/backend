cb2394840ebdb75ceab81ab49f7f7e0d1b0bab83
LUKASRIB15 <lucas.ribeiro06@aluno.ifce.edu.br>
2022-04-23 00:17:20 +0000

    Criação da rota cadastro_cliente no blueprint

--- backend/blueprints/cliente/cliente.py
+++ backend/blueprints/cliente/cliente.py
@@ -1,13 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from backend.models import Cliente
+from backend.ext.database import db
 
 
 bp = Blueprint('cliente', __name__, url_prefix='/cliente', template_folder='templates')
 
 
-@bp.route('/')
-def root():
-    return 'Hello from cliente'
+@bp.route("/cadastro_cliente", methods=["GET", "POST"])
+def cadastro_cliente():
+    if request.method == "POST":
+        novo = Cliente()
+        novo.nome = request.form["nome"]
+        novo.email = request.form["email"]
+        novo.senha = request.form["senha"]
 
+        db.session.add(novo)
+        db.session.commit()
 
+        return "Bem-vindo ao FoodFlash"
+    else:
+        return "Não deu certo"
+
+@bp.route("/inicio")
+def teste():
+    return "oi"
 def init_app(app):
     app.register_blueprint(bp)
