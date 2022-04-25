3cf5717bc68c1ef74e502bbd1bfb5e4bb5b33e79
Lucas Ribeiro <100320271+LUKASRIB15@users.noreply.github.com>
2022-04-25 19:03:40 -0300

    Cadastrocliente#1 (#38)
    * Criação da rota cadastro_cliente no blueprint
    
    * remoção da rota bp teste

--- backend/blueprints/cliente/cliente.py
+++ backend/blueprints/cliente/cliente.py
@@ -1,12 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from backend.models import Cliente
+from backend.ext.database import db
 
 
 bp = Blueprint('cliente', __name__, url_prefix='/cliente', template_folder='templates')
 
 
-@bp.route('/')
-def root():
-    return 'Hello from cliente'
+@bp.route("/cadastro_cliente", methods=["GET", "POST"])
+def cadastro_cliente():
+    if request.method == "POST":
+        novo = Cliente()
+        novo.nome = request.form["nome"]
+        novo.email = request.form["email"]
+        novo.senha = request.form["senha"]
+
+        db.session.add(novo)
+        db.session.commit()
+
+        return "Bem-vindo ao FoodFlash"
+    else:
+        return "Não deu certo"
 
 
 def init_app(app):
