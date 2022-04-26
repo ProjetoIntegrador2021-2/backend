075ad9dd799b7be770ce06c81eb2c1b908592f26
nicollesouza01 <100320256+nicollesouza01@users.noreply.github.com>
2022-04-25 21:34:34 -0300

    Tabela Entregador e rota blueprint cadastro Entregador (#39)

--- backend/blueprints/entregador/entregador.py
+++ backend/blueprints/entregador/entregador.py
@@ -1,12 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from backend.models import Entregador
+from backend.ext.database import db
+
 
 
 bp = Blueprint('entregador', __name__, url_prefix='/entregador', template_folder='templates')
 
 
-@bp.route('/')
-def root():
-    return 'Hello from entregador'
+@bp.route('/cadastro_entregador', methods=["GET", "POST"])
+def cadastro_entregador():
+    if request.method == "POST":
+        novo = Entregador()
+        novo.veiculo = request.form["veiculo"]
+        novo.regiao = request.form["regiao"]
+
+        db.session.add(novo)
+        db.session.commit()
+
+        return "Bem-vindo ao Flash Food, entregador! Venha gastar sua gasolina com a gente!"
+    else:
+        return "Dança da motinha"
+
 
 
 def init_app(app):
