d6e24bea29d9c79a356e58930fecfa2622f7e180
Athsxt <athos.rocha08@aluno.ifce.edu.br>
2022-04-27 22:24:09 +0000

    Criação da Tabela Restaurante e Rota Blueprint cadastro de Restaurante

--- backend/models.py
+++ backend/models.py
@@ -4,8 +4,13 @@ from flask_login import UserMixin
 
 
 @login_manager.user_loader
+
 def carregaCliente(id):
     return Cliente.query.get(id)
+
+def carregaEntregador(id):
+    return Entregador.query.get(id)
+
 
 class Cliente(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,12 +18,17 @@ class Cliente(db.Model, UserMixin):
     email = db.Column("Email", db.String(100), unique=True, nullable=False)
     senha = db.Column("Senha", db.String(100), nullable=False)
 
-def carregaEntregador(id):
-    return Entregador.query.get(id)
-
 class Entregador(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     veiculo = db.Column("Veiculo", db.Boolean, default=False)
     regiao = db.Column("Regiao", db.Boolean, default=False)
 
     cliente_id = db.Column(db.Integer, db.ForeignKey("cliente.id"))
+
+class Restaurante(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column('Nome', db.String(400), nullable=False)
+    email = db.Column('Email', db.String(100), unique=True, nullable=False)
+    senha = db.Column('Senha', db.String(100), nullable=False)
+    endereco = db.Column('Endereço', db.String(200), nullable=False)
+    cidade = db.Column('Cidade', db.Boolean, default=False)
--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from backend.models import Cliente
 from backend.models import Entregador
+from backend.models import Restaurante
 
 def root():
     return render_template("index.html")
