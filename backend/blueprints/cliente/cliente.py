c2ad07c9523d5bac5cadc183a2277590f24f5f74
LUKASRIB15 <lucas.ribeiro06@aluno.ifce.edu.br>
2022-04-29 22:25:14 +0000

    Rota de login de cliente

--- backend/blueprints/cliente/cliente.py
+++ backend/blueprints/cliente/cliente.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, request
+from flask_login import login_user
 from backend.models import Cliente
 from backend.ext.database import db
 
 
-bp = Blueprint('cliente', __name__, url_prefix='/cliente', template_folder='templates')
-
+bp = Blueprint('cliente', __name__, url_prefix="/cliente", template_folder='templates')
 
 @bp.route("/cadastro_cliente", methods=["GET", "POST"])
 def cadastro_cliente():
@@ -21,6 +21,19 @@ def cadastro_cliente():
     else:
         return "Não deu certo"
 
+@bp.route("/login_cliente", methods=["GET", "POST"])
+def login_cliente():
+    if request.method == "POST":
+        email = request.form["email"]
+        senha = request.form["senha"]
+
+        cliente = Cliente.query.filter_by(email=email).first()
+
+        if not cliente:
+            return "Cliente não cadastrado",404
+        if cliente.senha == senha:
+            login_user(cliente)
+            return "Deu certo!"
 
 def init_app(app):
     app.register_blueprint(bp)
